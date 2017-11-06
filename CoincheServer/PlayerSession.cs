78bf466b296df5db232215e91679f43c4aa04f1c
Hugo Soszynski <hugo.soszynski@epitech.eu>
2017-11-06 11:20:44 +0100

    Changing architecture

--- CoincheServer/PlayerSession.cs
+++ CoincheServer/PlayerSession.cs
@@ -10,40 +10,6 @@ namespace CoincheServer
 {
     class PlayerSession
     {
-        #region Attributes
-        private NetworkStream _Stream { get; set; }
-        public NetworkStream Stream { get { return _Stream; } }
-        public string Name
-        {
-            get { return Name; }
-            set
-            {
-                if (!String.IsNullOrEmpty(value) && !String.IsNullOrWhiteSpace(value))
-                    Name = value;
-            }
-        }
-        #endregion
-
-        #region Constructor
-        public PlayerSession(ref Socket sock)
-        {
-            _Stream = new NetworkStream(sock);
-        }
-        #endregion
-
-        #region Operators
-        public static bool operator ==(PlayerSession lhs, PlayerSession rhs)
-        {
-            return lhs.Stream.Equals(rhs.Stream);
-        }
-        public static bool operator !=(PlayerSession lhs, PlayerSession rhs)
-        {
-            return !lhs.Stream.Equals(rhs.Stream);
-        }
-        #endregion
-
-        #region Network Interactions
         
-        #endregion
     }
 }
