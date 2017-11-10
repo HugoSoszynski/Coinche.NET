c1c5b06e201ff6072408695f704ba5b45ef5d927
Maxime Pillon <maxime.pillon@epitech.eu>
2017-11-10 15:53:39 +0100

    Again without testing complete all the project need to debug certainly but all the code is present

---
+++ CoincheServer/Contract.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CoincheServer {
+
+    public class Contract {
+        public enum ContractType {
+            Spades,
+            Clubs,
+            Hearts,
+            Diamonds,
+            AllTrumps,
+            NoTrumps,
+        };
+
+        public ContractType type;
+        public int scale;
+        public int coinche;
+        public Team team;
+
+        public Contract(ContractType type, int scale, int coinche, Team team) {
+            this.type = type;
+            this.scale = scale;
+            this.coinche = coinche;
+            this.team = team;
+        }
+    }
+}
